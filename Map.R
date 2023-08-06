219816643d238181394457b4298d8687c7aab8ad
charnozari <charnoz@uw.edu>
2023-08-06 14:58:34 -0700

    FinishedFr

--- Map.R
+++ Map.R
@@ -1,25 +1,23 @@
-library("maps")
-library("ggplot2")
-library("dplyr")
-library("sf")
-
-# Load the shapefile for US states using the maps package
-usa_states <- map("state", plot = FALSE, fill = TRUE) %>% 
-  st_as_sf()
-
-# Assuming 'mean_black_pop_by_state' is your dataset
-# Convert the column name to uppercase to match the name in 'usa_states' (ID_1)
-mean_black_pop_by_state <- mean_black_pop_by_state %>%
-  rename(ID_1 = state) %>%
-  filter(!is.na(black_prison_pop))  # Remove rows with NA values
-
-# Merge the shapefile with your mean_black_pop_by_state data using left_join
-map_data <- usa_states %>%
-  left_join(mean_black_pop_by_state, by = "ID_1")
-
-# Create the map using ggplot2 and geom_sf
+dataset2 <- read.csv("https://github.com/melaniewalsh/Neat-Datasets/raw/main/us-prison-jail-rates-1990-WA.csv")
+library(tidyverse)
+library(sf)
+library(tigris)
+options(sf_use_implementation = FALSE)
+wa_counties <- counties(state = "WA", cb = TRUE)
+intermediate_data_path <- "C:/Users/charl/info201/assignment-04-charnozari/wa_counties.rds"
+if (!file.exists(intermediate_data_path)) {
+  wa_counties <- counties(state = "WA", cb = TRUE)
+  saveRDS(wa_counties, intermediate_data_path)
+} else {
+  wa_counties <- readRDS(intermediate_data_path)
+}
+avg_black_prison_rate_counties <- dataset2 %>%
+  group_by(county_name) %>%
+  summarize(avg_black_prison_pop_rate_counties = mean(black_prison_pop_rate, na.rm = TRUE))
+avg_black_prison_rate_counties <- rename(avg_black_prison_rate_counties, NAMELSAD = county_name)
+wa_counties_avg <- left_join(wa_counties, avg_black_prison_rate_counties, by = "NAMELSAD")
 ggplot() +
-  geom_sf(data = map_data, aes(fill = black_prison_pop)) +
-  scale_fill_gradient(name = "Mean Black Prison Pop") +
-  labs(title = "Mean Black Prison Population by State") +
+  geom_sf(data = wa_counties_avg, aes(fill = avg_black_prison_pop_rate_counties)) +
+  scale_fill_viridis_c(name = "Avg Black Prison Pop Rate") +
+  labs(title = "Average Black Prison Population Rate by County in WA") +
   theme_minimal()
--- summary.R
+++ summary.R
@@ -5,24 +5,23 @@ mean_aapi_prison_pop_rate <- mean(dataset$aapi_prison_pop_rate, na.rm = T)
 mean_latinx_prison_pop_rate <- mean(dataset$latinx_prison_pop_rate, na.rm = T)
 mean_native_prison_pop_rate <- mean(dataset$native_prison_pop_rate, na.rm = T)
 min_black_prison_pop_rate <- min(dataset$black_prison_pop_rate, na.rm = T)
-dataset1 <- read.csv("https://github.com/melaniewalsh/Neat-Datasets/blob/main/us-prison-pop.csv?raw=true")
 mean_white_prison_pop_rate_function <- function(){
   i <- mean_white_prison_pop_rate
-  return(i)
+  return(sprintf("%.2f", i))
 }
 mean_latinx_prison_pop_rate_function <- function(){
   i <- mean_latinx_prison_pop_rate
-  return(i)
+  return(sprintf("%.2f", i))
 }
 mean_aapi_prison_pop_rate_function <- function(){
   i <- mean_aapi_prison_pop_rate
-  return(i)
+  return(sprintf("%.2f", i))
 }
 mean_native_prison_pop_rate_function <- function(){
   i <- mean_native_prison_pop_rate
-  return(i)
+  return(sprintf("%.2f", i))
 }
 mean_black_prison_pop_rate_function <- function(){
   i <- mean_black_prison_pop_rate
-  return(i)
+  return(sprintf("%.2f", i))
 }
